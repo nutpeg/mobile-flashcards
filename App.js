d346f1e058f66e6e2817449a3611c416f6ba0d0a
Tim Jones <nutpeg@gmail.com>
2018-06-02 21:04:13 +0100

    feat: Add icons to tabs

--- App.js
+++ App.js
@@ -7,17 +7,49 @@ import {
 import AddDeck from './components/AddDeck';
 import Deck from './components/Deck';
 import Decks from './components/Decks';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { colors } from './utils/colors';
+import fakeData from './utils/fakeData';
 
 const DecksNav = createStackNavigator({
   Decks: { screen: Decks },
   Deck: { screen: Deck },
 });
 
-const Tabs = createBottomTabNavigator({
-  Decks: { screen: Decks },
-  AddDeck: { screen: AddDeck },
-});
+const Tabs = createBottomTabNavigator(
+  {
+    Decks: {
+      screen: Decks,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialCommunityIcons name="cards" size={30} color={tintColor} />
+        ),
+      },
+    },
+    AddDeck: {
+      screen: AddDeck,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialIcons name="add" size={30} color={tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    initialRouteName: 'Decks',
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      style: {
+        backgroundColor: colors.greyLight,
+      },
+      showLabel: true,
+      showIcon: true,
+      activeTintColor: colors.blue,
+      inactiveTintColor: colors.greyDark,
+      renderIndicator: () => null,
+    },
+  },
+);
 
 export default class App extends React.Component {
   state = {
