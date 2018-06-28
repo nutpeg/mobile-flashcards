85e33e3ba31e82655295c07dc6fc1ed8a874bf9d
Tim Jones <nutpeg@gmail.com>
2018-06-28 16:52:32 +0100

    refactor: Remove unused import

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AsyncStorage } from 'react-native';
-import { colors } from './utils/colors';
 import AppNav from './components/routes';
 import initialState from './utils/initialState';
 
