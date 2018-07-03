23ea075ddc81f52b295d6c990f5d3d5e41795544
Tim Jones <nutpeg@gmail.com>
2018-07-03 18:02:58 +0100

    chore: Prettier change

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AppNav from './config/routes';
 import store from './config/store';
 import { Provider } from 'react-redux';
-import { setLocalNotification } from "./api/notificationsAPI";
+import { setLocalNotification } from './api/notificationsAPI';
 
 export default class App extends React.Component {
   state = {};
